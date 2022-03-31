223f4a2c808d39838f1bc30a2188151cc19e2175
Rishic2002 <73890141+Rishic2002@users.noreply.github.com>
2022-03-31 11:41:59 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'signup.dart';
+import 'Profile.dart';
+import 'package:student_app/Profile.dart';
 
 final FirebaseAuth _auth = FirebaseAuth.instance;
 
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(const MyApp());
@@ -34,7 +36,7 @@ class MyApp extends StatelessWidget {
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
       debugShowCheckedModeBanner: false,
       routes: <String, WidgetBuilder>{
-        '/signup':(BuildContext context) => const SignupPage()
+        '/signup': (BuildContext context) => const SignupPage()
       },
     );
   }
@@ -59,16 +61,19 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
   int _success = 1;
   String _userEmail = "";
 
-  void _singIn() async {
-    final User? user = (await _auth.signInWithEmailAndPassword(email: _emailController.text, password: _passwordController.text)).user;
+  get Profile => null;
 
-    if(user != null) {
+  void _singIn() async {
+    final User? user = (await _auth.signInWithEmailAndPassword(
+            email: _emailController.text, password: _passwordController.text))
+        .user;
+
+    if (user != null) {
       setState(() {
         _success = 2;
         _userEmail = user.email!;
@@ -91,64 +96,54 @@ class _MyHomePageState extends State<MyHomePage> {
               children: <Widget>[
                 Container(
                   padding: const EdgeInsets.fromLTRB(15, 110, 0, 0),
-                  child: const Text("Welcome!",
-                  style: TextStyle(
-                    fontSize: 40,
-                    fontWeight: FontWeight.bold
-                  ),),
+                  child: const Text(
+                    "Welcome!",
+                    style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+                  ),
                 )
               ],
             ),
           ),
           Container(
-            padding: const EdgeInsets.only(top:35,left: 20,right: 30),
+            padding: const EdgeInsets.only(top: 35, left: 20, right: 30),
             child: Column(
               children: <Widget>[
                 TextField(
                   controller: _emailController,
                   decoration: const InputDecoration(
-                    labelText:"Email",
-                    labelStyle: TextStyle(
-                      fontFamily: 'Montserrat',
-                      color: Colors.grey
-                    ),
-                    focusedBorder: UnderlineInputBorder(
-                      borderSide: BorderSide(
-                        color: Colors.green
-                      )
-                    )
-                  ),
+                      labelText: "Email",
+                      labelStyle: TextStyle(
+                          fontFamily: 'Montserrat', color: Colors.grey),
+                      focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.green))),
                 ),
-                const SizedBox(height: 20,),
+                const SizedBox(
+                  height: 20,
+                ),
                 TextField(
                   controller: _passwordController,
                   decoration: const InputDecoration(
-                      labelText:"Password",
+                      labelText: "Password",
                       labelStyle: TextStyle(
-                          fontFamily: 'Montserrat',
-                          color: Colors.grey
-                      ),
+                          fontFamily: 'Montserrat', color: Colors.grey),
                       focusedBorder: UnderlineInputBorder(
-                          borderSide: BorderSide(
-                              color: Colors.green
-                          )
-                      )
-                  ),
+                          borderSide: BorderSide(color: Colors.green))),
                   obscureText: true,
                 ),
-                const SizedBox(height: 5,),
+                const SizedBox(
+                  height: 5,
+                ),
                 Container(
                   alignment: const Alignment(1, 0),
-                  padding: const EdgeInsets.only(top: 15,left: 20),
+                  padding: const EdgeInsets.only(top: 15, left: 20),
                   child: const InkWell(
                     child: Text(
                       "Forgot Password",
                       style: TextStyle(
-                        color: Colors.black,
-                        fontWeight: FontWeight.bold,
-                        fontFamily: "Montserrat",
-                        decoration: TextDecoration.underline
-                      ),
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                          fontFamily: "Montserrat",
+                          decoration: TextDecoration.underline),
                     ),
                   ),
                 ),
@@ -158,14 +153,14 @@ class _MyHomePageState extends State<MyHomePage> {
                     child: Text(
                       _success == 1
                           ? ''
-                          : (
-                          _success == 2
+                          : (_success == 2
                               ? 'Successfully signed in ' + _userEmail
                               : 'Sign in failed'),
                       style: TextStyle(color: Colors.red),
-                    )
+                    )),
+                const SizedBox(
+                  height: 40,
                 ),
-                const SizedBox(height: 40,),
                 Container(
                   height: 40,
                   child: Material(
@@ -174,8 +169,15 @@ class _MyHomePageState extends State<MyHomePage> {
                     color: Colors.black,
                     elevation: 7,
                     child: GestureDetector(
-                      onTap: () async{
+                      onTap: () async {
                         _singIn();
+                        onPressed:
+                        () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (_) => Profile.dart()));
+                        };
                       },
                       child: const Center(
                         child: Text(
@@ -190,22 +192,23 @@ class _MyHomePageState extends State<MyHomePage> {
                     ),
                   ),
                 ),
-                const SizedBox(height: 15,),
+                const SizedBox(
+                  height: 15,
+                ),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
                     InkWell(
-                      onTap: (){
+                      onTap: () {
                         Navigator.of(context).pushNamed('/signup');
                       },
                       child: const Text(
                         'Register',
                         style: TextStyle(
-                          color: Colors.blueGrey,
-                          fontFamily: 'Montserrat',
-                          fontWeight: FontWeight.bold,
-                          decoration: TextDecoration.underline
-                        ),
+                            color: Colors.blueGrey,
+                            fontFamily: 'Montserrat',
+                            fontWeight: FontWeight.bold,
+                            decoration: TextDecoration.underline),
                       ),
                     )
                   ],
